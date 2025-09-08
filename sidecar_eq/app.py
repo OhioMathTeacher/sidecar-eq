b514f6b4b88046e077c13536b423ca2b26cefe4f
Michael Todd Edwards <michael.todd.edwards@gmail.com>
2025-09-07 22:51:03 -0400

    Update app.py

--- sidecar_eq/app.py
+++ sidecar_eq/app.py
@@ -28,6 +28,10 @@ class MainWindow(QMainWindow):
         self.table.setSelectionMode(QTableView.ExtendedSelection)
         self.setCentralWidget(self.table)
 
+        header = self.table.horizontalHeader()
+        header.setStretchLastSection(True)
+        self.table.setAlternatingRowColors(False)
+
         self._build_toolbar()
         self.statusBar().showMessage("Ready")
 
