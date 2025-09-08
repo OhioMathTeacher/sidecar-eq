ce5682fd8ef0d920700a4be517746868340c82e2
Michael Todd Edwards <michael.todd.edwards@gmail.com>
2025-09-08 13:01:57 -0400

    Update app.py

--- sidecar_eq/app.py
+++ sidecar_eq/app.py
@@ -47,7 +47,7 @@ class MainWindow(QMainWindow):
         sb.showMessage("Ready")
 
         # time “knob
-        thermometer-style progress bar
+        # thermometer-style progress bar
         self.progress = QProgressBar()
         self.progress.setTextVisible(False)
         self.progress.setRange(0, 0)    # 0/0 until we know the duration
